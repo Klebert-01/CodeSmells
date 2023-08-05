8755d0c3814fd6b0342a434e0103f17aa4d3cd00
Klebert-01 <arvid.berkert@gmail.com>
2023-08-05 17:39:56 +0200

    moved game logic and player data to separate class lib

--- MooGame/Program.cs
+++ MooGame/Program.cs
@@ -1,13 +1,14 @@
 ﻿namespace MooGame;
 
+using GameResources;
 using System;
 using System.Collections.Generic;
 using System.IO;
 
 class MainClass
-    {
+{
 
-        public static void Main(string[] args)
+    public static void Main(string[] args)
         {
 
         bool playOn = true;
@@ -19,24 +20,24 @@ class MainClass
 
         while (playOn)
             {
-                //
-                string goal = makeGoal();
+                
+                string goal = MooGameLogic.makeGoal();
 
 
                 Console.WriteLine("New game:\n");
                 //comment out or remove next line to play real games!
                 Console.WriteLine("For practice, number is: " + goal + "\n");
-                string guess = Console.ReadLine();
+                string guess = Console.ReadLine();  // TODO parse to int directly
 
                 int nGuess = 1;
-                string bbcc = checkBC(goal, guess);
+                string bbcc = MooGameLogic.checkBC(goal, guess);
                 Console.WriteLine(bbcc + "\n");
                 while (bbcc != "BBBB,")
                 {
                     nGuess++;
                     guess = Console.ReadLine();
                     Console.WriteLine(guess + "\n");
-                    bbcc = checkBC(goal, guess);
+                    bbcc = MooGameLogic.checkBC(goal, guess);
                     Console.WriteLine(bbcc + "\n");
                 }
                 StreamWriter output = new StreamWriter("result.txt", append: true);
@@ -51,62 +52,21 @@ class MainClass
                 }
             }
         }
-        static string makeGoal()    //does a lot of things, extract to smaller methods 
-        {
-            Random randomGenerator = new Random(); //creates new random object
-            string goal = "";   
-            for (int i = 0; i < 4; i++)
-            {
-                int random = randomGenerator.Next(10); 
-                string randomDigit = "" + random;
-
-                while (goal.Contains(randomDigit))
-                {
-                    random = randomGenerator.Next(10);
-                    randomDigit = "" + random;
-                }
-                goal = goal + randomDigit; // change to += operator
-            }
-            return goal;
-        }
-
-        static string checkBC(string goal, string guess)
-        {
-            int cows = 0, bulls = 0;
-            guess += "    ";     // if player entered less than 4 chars
-            for (int i = 0; i < 4; i++)
-            {
-                for (int j = 0; j < 4; j++)
-                {
-                    if (goal[i] == guess[j])
-                    {
-                        if (i == j)
-                        {
-                            bulls++;
-                        }
-                        else
-                        {
-                            cows++;
-                        }
-                    }
-                }
-            }
-            return "BBBB".Substring(0, bulls) + "," + "CCCC".Substring(0, cows);
-        }
 
 
-        static void showTopList() 
-        {
+        static void showTopList() //method for getting and writing highscores
+    {
 
-        StreamReader input = new StreamReader("result.txt"); //make each high-score post an object then serialize to JSON instead
+        StreamReader input = new StreamReader("result.txt");
             List<PlayerData> results = new List<PlayerData>();
             string line;
-            while ((line = input.ReadLine()) != null)
+            while ((line = input.ReadLine()) != null)   
             {
 
-                string[] nameAndScore = line.Split(new string[] { "#&#" }, StringSplitOptions.None);
+                string[] nameAndScore = line.Split(new string[] { "#&#" }, StringSplitOptions.None); //weird to store name and score as array and not separate datatypes
                 string name = nameAndScore[0];
                 int guesses = Convert.ToInt32(nameAndScore[1]);
+
                 PlayerData pd = new PlayerData(name, guesses);
                 int pos = results.IndexOf(pd);
                 if (pos < 0)
@@ -129,45 +89,6 @@ class MainClass
                 Console.WriteLine(string.Format("{0,-9}{1,5:D}{2,9:F2}", p.Name, p.NGames, p.Average()));
             }
             input.Close();
-        }
-    }
-
-    class PlayerData
-    {
-
-        public string Name { get; private set; }
-        public int NGames { get; private set; }
-        int totalGuess;
-
-
-        public PlayerData(string name, int guesses)
-        {
-            this.Name = name;
-            NGames = 1;
-            totalGuess = guesses;
-        }
-
-        public void Update(int guesses)
-        {
-            totalGuess += guesses;
-            NGames++;
-        }
-
-        public double Average()
-        {
-            return (double)totalGuess / NGames;
-        }
-
-
-        public override bool Equals(Object p)
-        {
-            return Name.Equals(((PlayerData)p).Name);
-        }
-
-
-        public override int GetHashCode()
-        {
-            return Name.GetHashCode();
         }
     }
 
