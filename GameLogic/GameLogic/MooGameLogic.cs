2d312737f4e147c5b137c7e9d2355d8792e6508f
Klebert-01 <arvid.berkert@gmail.com>
2023-08-05 17:51:54 +0200

    Moved showTopList method to gamelogic class

--- GameLogic/GameLogic/MooGameLogic.cs
+++ GameLogic/GameLogic/MooGameLogic.cs
@@ -1,4 +1,6 @@
-﻿namespace GameResources.GameLogic;
+﻿using GameResources.Data;
+
+namespace GameResources.GameLogic;
 
 public static class MooGameLogic
 {
@@ -46,5 +48,42 @@ public static class MooGameLogic
             }
         }
         return "BBBB".Substring(0, bulls) + "," + "CCCC".Substring(0, cows);
+    }
+
+    public static void showTopList() //method for getting and writing highscores
+    {
+
+        StreamReader input = new StreamReader("result.txt");
+        List<PlayerData> results = new List<PlayerData>();
+        string line;
+        while ((line = input.ReadLine()) != null)
+        {
+
+            string[] nameAndScore = line.Split(new string[] { "#&#" }, StringSplitOptions.None); //weird to store name and score as array and not separate datatypes
+            string name = nameAndScore[0];
+            int guesses = Convert.ToInt32(nameAndScore[1]);
+
+            PlayerData pd = new PlayerData(name, guesses);
+            int pos = results.IndexOf(pd);
+            if (pos < 0)
+            {
+                results.Add(pd);
+            }
+            else
+            {
+                results[pos].Update(guesses);
+            }
+
+
+        }
+
+
+        results.Sort((p1, p2) => p1.Average().CompareTo(p2.Average()));
+        Console.WriteLine("Player   games average");
+        foreach (PlayerData p in results)
+        {
+            Console.WriteLine(string.Format("{0,-9}{1,5:D}{2,9:F2}", p.Name, p.NGames, p.Average()));
+        }
+        input.Close();
     }
 }
--- MooGame/Program.cs
+++ MooGame/Program.cs
@@ -38,7 +38,7 @@ class MainClass
             StreamWriter output = new StreamWriter("result.txt", append: true);
             output.WriteLine(name + "#&#" + nGuess);
             output.Close();
-            showTopList();
+            MooGameLogic.showTopList();
             Console.WriteLine("Correct, it took " + nGuess + " guesses\nContinue?");
             string answer = Console.ReadLine();
             if (answer != null && answer != "" && answer.Substring(0, 1) == "n")
@@ -46,44 +46,6 @@ class MainClass
                 playOn = false;
             }
         }
-    }
-
-
-    static void showTopList() //method for getting and writing highscores
-    {
-
-        StreamReader input = new StreamReader("result.txt");
-        List<PlayerData> results = new List<PlayerData>();
-        string line;
-        while ((line = input.ReadLine()) != null)
-        {
-
-            string[] nameAndScore = line.Split(new string[] { "#&#" }, StringSplitOptions.None); //weird to store name and score as array and not separate datatypes
-            string name = nameAndScore[0];
-            int guesses = Convert.ToInt32(nameAndScore[1]);
-
-            PlayerData pd = new PlayerData(name, guesses);
-            int pos = results.IndexOf(pd);
-            if (pos < 0)
-            {
-                results.Add(pd);
-            }
-            else
-            {
-                results[pos].Update(guesses);
-            }
-
-
-        }
-
-
-        results.Sort((p1, p2) => p1.Average().CompareTo(p2.Average()));
-        Console.WriteLine("Player   games average");
-        foreach (PlayerData p in results)
-        {
-            Console.WriteLine(string.Format("{0,-9}{1,5:D}{2,9:F2}", p.Name, p.NGames, p.Average()));
-        }
-        input.Close();
     }
 }
 
