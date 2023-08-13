57926290e70da60a7da57ddebeee76a116fe3de1
Klebert-01 <arvid.berkert@gmail.com>
2023-08-13 15:33:12 +0200

    added todo comment, bug on correct answer seems to have dissappeared. why, i dont know

--- MooGame/DataAccess/HighscoreManager.cs
+++ MooGame/DataAccess/HighscoreManager.cs
@@ -2,9 +2,7 @@
 
 public class HighscoreManager
 {
-
-
-    public void GetAndDisplayHighscore() //separate into two methods
+    public void GetAndDisplayHighscore() //TODO separate into two methods, Get method returning list of results, display highscore should be in View/IO class? 
     {
         using (var sr = new StreamReader("result.txt"))
         {
@@ -28,10 +26,7 @@ public class HighscoreManager
                 {
                     results[pos].Update(guesses);
                 }
-
-
             }
-
 
             results.Sort((p1, p2) => p1.Average().CompareTo(p2.Average()));
             Console.WriteLine("Player   games average");
@@ -41,8 +36,6 @@ public class HighscoreManager
             }
         }
     }
-
-
     public void AddHighscore(string userName, int nGuess)   // TODO should just accept a playerdata object? or a list of playerdata?
     {
         using (var sw = new StreamWriter("result.txt", append: true))
@@ -50,47 +43,6 @@ public class HighscoreManager
             sw.WriteLine($"{userName}#&#{nGuess}");
         }
     }
-
-
-    // maybe a update highscore method also?
-
-
+    // TODO maybe an update highscore method also?
 }
 
-//public static void DisplayHighscore()
-//{
-
-//    StreamReader input = new StreamReader("result.txt"); //store as json instead
-//    List<PlayerData> results = new List<PlayerData>();
-//    string line;
-//    while ((line = input.ReadLine()) != null)
-//    {
-
-//        string[] nameAndScore = line.Split(new string[] { "#&#" }, StringSplitOptions.None); //weird to store name and score as array and not separate datatypes
-//        string name = nameAndScore[0];
-//        int guesses = Convert.ToInt32(nameAndScore[1]);
-
-//        PlayerData pd = new PlayerData(name, guesses);
-//        int pos = results.IndexOf(pd);
-//        if (pos < 0)
-//        {
-//            results.Add(pd);
-//        }
-//        else
-//        {
-//            results[pos].Update(guesses);
-//        }
-
-
-//    }
-
-
-//    results.Sort((p1, p2) => p1.Average().CompareTo(p2.Average()));
-//    Console.WriteLine("Player   games average");
-//    foreach (PlayerData p in results)
-//    {
-//        Console.WriteLine(string.Format("{0,-9}{1,5:D}{2,9:F2}", p.Name, p.GamesPlayed, p.Average()));
-//    }
-//    input.Close();
-//}
-//}
