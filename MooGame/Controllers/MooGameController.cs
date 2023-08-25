c4cdae08099aa1420a8821e80e59fbba72b35d2b
Klebert-01 <arvid.berkert@gmail.com>
2023-08-25 11:09:43 +0200

    started implementing game controller

--- MooGame/Controllers/MooGameController.cs
+++ MooGame/Controllers/MooGameController.cs
@@ -4,9 +4,18 @@ namespace MooGame.Controllers;
 
 public class MooGameController
 {
+    private IHighscoreManager _highscoreManager;
+    private IMooGameLogic _mooGameLogic;
+    private IUI _ui;
+    public MooGameController(IHighscoreManager highscoreManager, IMooGameLogic mooGameLogic, IUI ui)
+    {
+        _highscoreManager = highscoreManager;
+        _mooGameLogic = mooGameLogic;
+        _ui = ui;
+    }
 
-	public MooGameController()
-	{
+    public void StartGame()
+    {
 
-	}
+    }
 }
